05505491ab0d3bd63b3f79c75114b14d7c40d8c7
Joonas Kauppinen <joonas.kauppinen@yle.fi>
2020-03-28 19:11:01 +0200

    Add cors

--- week2/app.js
+++ week2/app.js
@@ -1,11 +1,14 @@
 "use strict";
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const cat = require("./routes/catRoute.js");
 const user = require("./routes/userRoute");
 const port = 3000;
 
+app.use(cors());
 app.use("/cat", cat);
 app.use("/user", user);
+app.use("/static", express.static("public_html"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
