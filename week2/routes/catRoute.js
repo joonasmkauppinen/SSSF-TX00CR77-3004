bfecdb9e98a2dfc4af9abc96300627b0aa09fd8c
Joonas Kauppinen <joonas.kauppinen@yle.fi>
2020-03-28 19:39:43 +0200

    Add multer

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -1,13 +1,15 @@
 "use strict";
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const catController = require("../controllers/catController.js");
 
 router.get("/", catController.cat_list_get);
 
 router.get("/:id", catController.cat_get);
 
-router.post("/", (req, res) => {
+router.post("/", upload.single("cat"), (req, res) => {
   res.send("With this endpoint you can add cats.");
 });
 
